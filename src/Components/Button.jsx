723e15636a5f981a38feb5d7664f187c8eb36022
Colton Neves <colton@simcase.io>
2019-04-08 10:02:56 -0400

    first commit

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -5,7 +5,7 @@ export class Button extends React.Component {
     render() {
         return(
             <button 
-                onClick={() => this.props.onClick()}>
+                onClick={() => this.props.onClick}>
                 {this.props.title}</button>
         );
     }
