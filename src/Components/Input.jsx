42bb7f9f8434568391039733a578f46496d946ac
Colton Neves <colton@simcase.io>
2019-04-10 09:30:21 -0400

    adding inputtitle method

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export class Input extends React.Component {
+    handleChange(e) {
+        const title = e.target.value;
+        this.props.inputTitle(title);
+    }
 
     render() {
         return(
-            <input></input>
+            <input onChange={this.handleChange}></input>
         )
     }
 }
